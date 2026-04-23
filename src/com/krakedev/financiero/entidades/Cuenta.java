8a201eefc70599b4fdc76aae327b84e4d01eb978
angel-hub31 <moralesmales1@hotmail.com>
2026-04-22 20:43:09 -0500

    Creacion de atributos, contructor y metodo imprimir en la clase Cuenta

---
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -0,0 +1,44 @@
+package com.krakedev.financiero.entidades;
+
+public class Cuenta {
+	private String id;
+	private double saldoActual;
+	private String tipo;
+	
+	
+	
+	
+	public Cuenta(String id) {
+
+		this.id = id;
+		this.saldoActual=0;
+		this.tipo="A";
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public double getSaldoActual() {
+		return saldoActual;
+	}
+	public void setSaldoActual(double saldoActual) {
+		this.saldoActual = saldoActual;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public void imprimir () {
+		System.out.println("Cuenta: "+id+",SaldoActual: "+saldoActual+", Tipo; "+tipo);
+		
+	}
+	
+	
+	
+
+}
