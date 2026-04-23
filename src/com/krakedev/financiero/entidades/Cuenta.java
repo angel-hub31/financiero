a80b304ce7993c58bf4cd7ab8e4f911c524c4ec5
angel-hub31 <moralesmales1@hotmail.com>
2026-04-22 21:05:54 -0500

    Se crea el crea el TestCuenta y tambien se modifica el metodo imprimir

--- src/com/krakedev/financiero/entidades/Cuenta.java
+++ src/com/krakedev/financiero/entidades/Cuenta.java
@@ -5,41 +5,49 @@ public class Cuenta {
 	private double saldoActual;
 	private String tipo;
 	private Cliente propietario;
-	
-	
-	
-	
+
 	public Cuenta(String id) {
 
 		this.id = id;
-		this.saldoActual=0;
-		this.tipo="A";
+		this.saldoActual = 0;
+		this.tipo = "A";
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public double getSaldoActual() {
 		return saldoActual;
 	}
+
 	public void setSaldoActual(double saldoActual) {
 		this.saldoActual = saldoActual;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
-	
-	public void imprimir () {
-		System.out.println("Cuenta: "+id+",SaldoActual: "+saldoActual+", Tipo; "+tipo);
-		
+
+	public Cliente getPropietario() {
+		return propietario;
 	}
-	
-	
-	
+
+	public void setPropietario(Cliente propietario) {
+		this.propietario = propietario;
+	}
+
+	public void imprimir() {
+		System.out.println("Cuenta: " + id + " | SaldoActual: " + saldoActual + " | Tipo: " + tipo);
+		System.out.println("Cedula cliente: "+propietario.getCedula());
+	}
 
 }
---
+++ src/com/krakedev/financiero/entidades/test/TestCuenta.java
@@ -0,0 +1,14 @@
+package com.krakedev.financiero.entidades.test;
+
+import com.krakedev.financiero.entidades.Cuenta;
+
+public class TestCuenta {
+
+	public static void main(String[] args) {
+		Cuenta c=new Cuenta("001");
+		c.imprimir();
+		
+		
+	}
+
+}
