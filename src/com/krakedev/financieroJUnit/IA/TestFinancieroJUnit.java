fa4702905ef224ce47096e1ebf01c4bb966ad473
angel-hub31 <moralesmales1@hotmail.com>
2026-04-22 23:42:47 -0500

    Pruebas unitarias 2: testDepositarMontoValido2

--- src/com/krakedev/financieroJUnit/IA/TestFinancieroJUnit.java
+++ src/com/krakedev/financieroJUnit/IA/TestFinancieroJUnit.java
@@ -1,6 +1,7 @@
 package com.krakedev.financieroJUnit.IA;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +20,17 @@ public class TestFinancieroJUnit {
 
         assertEquals("1000", cuenta1.getId());
         assertEquals("1001", cuenta2.getId());
+    }
+	@Test
+    void testDepositarMontoValido() {
+        Banco banco = new Banco();
+        Cliente cliente = new Cliente("1712345678", "Juan", "Perez");
+        Cuenta cuenta = banco.crearCuenta(cliente);
+        
+        boolean resultado = banco.depositar(100.0, cuenta);
+
+        assertTrue(resultado);
+        assertEquals(100.0, cuenta.getSaldoActual());
     }
 
 }
